8aea526176592511581ddbeb6f3bb96ce072cc91
Tyler Lubeck <tyler@tylerlubeck.com>
2017-04-20 17:28:45 -0700

    Remove the NullHandler patch because we don't support any python versions that need it

--- wukong/__init__.py
+++ wukong/__init__.py
@@ -1,11 +1,5 @@
 # Set up a null roothandler for our logging system
 
 import logging
-try:  # Python 2.7+
-    from logging import NullHandler
-except ImportError:
-    class NullHandler(logging.Handler):
-        def emit(self, record):
-            pass
-
+from logging import NullHandler
 logging.getLogger(__name__).addHandler(NullHandler())
